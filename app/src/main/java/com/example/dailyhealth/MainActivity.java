84b3cc5d12500a723bf0e51c97868722e190278c
KhoiPTA <tonysquadgamer@gmail.com>
2023-07-03 23:51:27 +0700

    <DEV> Add Moon Setting Screen 3 & 4

--- app/src/main/java/com/example/dailyhealth/MainActivity.java
+++ app/src/main/java/com/example/dailyhealth/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.dailyhealth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.NumberPicker;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/dailyhealth/MoonSettingScreen2.java
+++ app/src/main/java/com/example/dailyhealth/MoonSettingScreen2.java
@@ -3,6 +3,8 @@ package com.example.dailyhealth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.NumberPicker;
+import android.widget.Toast;
 
 public class MoonSettingScreen2 extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MoonSettingScreen2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_moon_setting_screen2);
+
+        NumberPicker MoonCyclePicker = findViewById(R.id.MoonCyclePickerButton);
+        if (MoonCyclePicker != null) {
+            MoonCyclePicker.setMinValue(25);
+            MoonCyclePicker.setMaxValue(40);
+            MoonCyclePicker.setWrapSelectorWheel(true);
+            MoonCyclePicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+                @Override
+                public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                    String text = "Changed from " + oldVal + " to " + newVal;
+                    Toast.makeText(MoonSettingScreen2.this, text, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
     }
 }
