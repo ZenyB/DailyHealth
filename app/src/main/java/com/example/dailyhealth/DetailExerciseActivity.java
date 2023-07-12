421b9b8ca037b22e0efaf16ab668e793e8fc3def
nyb1711 <nyb1711@gmail.com>
2023-07-12 23:08:12 +0700

    add: exercise2 UI

---
+++ app/src/main/java/com/example/dailyhealth/DetailExerciseActivity.java
@@ -0,0 +1,30 @@
+package com.example.dailyhealth;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class DetailExerciseActivity extends AppCompatActivity {
+
+    private ArrayList<SmallExercise> arrayList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_exercise);
+
+        getSupportActionBar().hide();
+
+        arrayList.add(new SmallExercise("Bài tập chính", "5 phút"));
+        arrayList.add(new SmallExercise("Bài tập cơ tay", "5 phút"));
+
+        final RecyclerView r = (RecyclerView) findViewById(R.id.detailExerciseRecyclerView);
+        r.setLayoutManager(new LinearLayoutManager(this));
+        //Toast.makeText(getApplicationContext(), arrayList.size(), Toast.LENGTH_SHORT).show();
+        r.setAdapter(new SmallExerciseAdapter(this, arrayList));
+    }
+}
