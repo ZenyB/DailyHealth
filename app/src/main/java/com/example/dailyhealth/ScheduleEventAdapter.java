e5d0ec0b354e52ae9df368261ba62ba98dc4f52f
KhoiPTA <tonysquadgamer@gmail.com>
2023-07-24 05:34:17 +0700

    <DEV> Add schedule events setting and some things

--- app/src/main/java/com/example/dailyhealth/ScheduleEventAdapter.java
+++ app/src/main/java/com/example/dailyhealth/ScheduleEventAdapter.java
@@ -2,11 +2,14 @@ package com.example.dailyhealth;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -32,24 +35,51 @@ public class ScheduleEventAdapter extends RecyclerView.Adapter<ScheduleEventAdap
     public ScheduleEventAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.schedule_event, parent, false);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
         return new ScheduleEventAdapter.ViewHolder(view, onItemClick);
     }
+
 
     @SuppressLint("ResourceAsColor")
     @Override
     public void onBindViewHolder(@NonNull ScheduleEventAdapter.ViewHolder holder, int position) {
         ScheduleEvent scheduleEvent = scheduleEvents.get(position);
-        if(position % 2 != 0){
+        position = holder.getAdapterPosition();
+        if(position % 2 != 0) {
             holder.constraintLayout.setBackgroundResource(R.drawable.rounded_white);
             holder.titleTextView.setTextColor(R.color.black);
             holder.detailTextView.setTextColor(R.color.black);
             holder.locationTextView.setTextColor(R.color.black);
             holder.imgView.setImageResource(R.drawable.location_img);
+            holder.delBut.setBackgroundResource(R.drawable.ic_x_black_foreground);
+        }
+        else {
+            holder.constraintLayout.setBackgroundResource(R.drawable.rounded_green);
+            holder.titleTextView.setTextColor(Color.parseColor("#FFFFFF"));
+            holder.detailTextView.setTextColor(Color.parseColor("#FFFFFF"));
+            holder.locationTextView.setTextColor(Color.parseColor("#FFFFFF"));
+            holder.imgView.setImageResource(R.drawable.location_white);
+            holder.delBut.setBackgroundResource(R.drawable.ic_x_white_foreground);
         }
         holder.titleTextView.setText(scheduleEvent.getTitle());
         holder.timeTextView.setText(scheduleEvent.getTime());
         holder.detailTextView.setText(scheduleEvent.getDetail());
         holder.locationTextView.setText(scheduleEvent.getLocation());
+        int finalPosition = position;
+        holder.delBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(activity, Integer.toString(holder.getPosition()), Toast.LENGTH_SHORT).show();
+                scheduleEvents.remove(holder.getPosition());
+                notifyItemRemoved(holder.getPosition());
+                notifyItemRangeChanged(holder.getPosition(), scheduleEvents.size() - holder.getPosition());
+            }
+        });
 
 
 //        holder.setItemClickListener(new ItemClickListener() {
@@ -74,17 +104,20 @@ public class ScheduleEventAdapter extends RecyclerView.Adapter<ScheduleEventAdap
         TextView titleTextView, detailTextView, timeTextView, locationTextView;
         private ItemClickListener itemClickListener;
         ConstraintLayout constraintLayout;
+        Button delBut;
         ImageView imgView;
         private ScheduleEventAdapter.OnItemClick onItemClick;
 
         public ViewHolder(@NonNull View itemView, ScheduleEventAdapter.OnItemClick onItemClick) {
             super(itemView);
+            delBut = itemView.findViewById(R.id.scheduleEventDelete);
             constraintLayout = itemView.findViewById((R.id.scheduleEventBorder));
             titleTextView = itemView.findViewById(R.id.scheduleEventTitleTV);
             detailTextView = itemView.findViewById(R.id.scheduleEventDetailTV);
             timeTextView = itemView.findViewById(R.id.scheduleTimeTV);
             locationTextView = itemView.findViewById(R.id.scheduleEventLocationTV);
             imgView = itemView.findViewById(R.id.imageView3);
+
             this.onItemClick = onItemClick;
 
             itemView.setOnClickListener(this);
