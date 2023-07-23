c808609eecaa8380e29b9b6fd28836c8a688d9e8
KailLow <21521551@gm.uit.edu.vn>
2023-07-24 04:44:27 +0700

    add Database User

--- app/src/main/java/com/example/dailyhealth/UserSetting.java
+++ app/src/main/java/com/example/dailyhealth/UserSetting.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,5 +16,8 @@ public class UserSetting extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_user);
+
+        EditText name = findViewById(R.id.userNameText);
+        EditText birth = findViewById(R.id.userBirthText);
     }
 }
