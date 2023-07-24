8919b3e0f4edf57c260b42b9727d83d64ae32980
KhoiPTA <tonysquadgamer@gmail.com>
2023-07-25 03:18:31 +0700

    <DEV> MoonCalendar UI update and Moon Setting update

--- app/src/main/java/com/example/dailyhealth/MoonSettingScreen3.java
+++ app/src/main/java/com/example/dailyhealth/MoonSettingScreen3.java
@@ -2,7 +2,9 @@ package com.example.dailyhealth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.NumberPicker;
 import android.widget.Toast;
 
@@ -25,5 +27,20 @@ public class MoonSettingScreen3 extends AppCompatActivity {
                 }
             });
         }
+    }
+
+    public void goBackBtn(View view) {
+        finish();
+    }
+    public void confirmBtn(View view)
+    {
+        Intent i = new Intent(getBaseContext(), MoonSettingScreen4.class);
+        startActivity(i);
+    }
+
+    public void notRemember(View view)
+    {
+        Intent i = new Intent(getBaseContext(), MoonSettingScreen4.class);
+        startActivity(i);
     }
 }
